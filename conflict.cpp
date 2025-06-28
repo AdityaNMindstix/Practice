c27ed2e0e558629fc0a7285b4973cb3547faef9c
AdityaNMindstix <aditya.navale@mindstix.com>
2025-06-28 09:41:36 +0530

    Add : k Variable

--- conflict.cpp
+++ conflict.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 int main(){
     int i = 0;
+    int k = 10;
     for( i =0 ; i<10 ; i++){
         cout<<i<<" ";
+        k--;
     }
+    coutM<<k<<" ";
     cout<<i<<" ";
 }
