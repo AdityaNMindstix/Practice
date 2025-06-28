954255ce3631cb8e03354faef17fb33489b53976
AdityaNMindstix <aditya.navale@mindstix.com>
2025-06-28 09:32:10 +0530

    Add : Count variable

--- conflict.cpp
+++ conflict.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 int main(){
     int i = 0;
+    int z = 1;
     for( i =0 ; i<10 ; i++){
         cout<<i<<" ";
+        z++;
     }
-    cout<<i<<" ";
+
+    cout<<z<<i<<" ";
 }
